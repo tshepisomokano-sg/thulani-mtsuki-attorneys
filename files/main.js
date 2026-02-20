40cb1745c004c31ad9b3edb1bb4367936076a4b5
tshepisomokano-sg <tshepisomokano@gmail.com>
2026-02-20 09:57:27 +0200

    Delete files directory

--- files/main.js
+++
@@ -1,6 +0,0 @@
-// ─── FORM SUBMISSION ───
-function handleSubmit(e) {
-  e.preventDefault();
-  alert('Thank you for your message. Thulani Mtsuki Attorneys will be in touch shortly.');
-  e.target.reset();
-}
